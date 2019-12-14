b19fdcb0bb8aa432ac38b0544d4abbe992d1be70
Shangming.Yang <shangming.yang@shopee.com>
2019-12-14 16:20:07 +0800

    modify base_operation.py

--- 01-start/base_operation.md
+++ 01-start/base_operation.md
@@ -3,6 +3,9 @@
 print(3 + 2) # 加法
 print(3 - 1) # 减法
 print(3 * 2) # 乘法
+print(3 / 2) # 除法
+print( 3  // 2) # 整除
+print( 3 % 2) # 取余数
 print(3 ** 2) ## 幂次计算
 print("abc" * 3) ## abc打印三次
 print("abc" + "abc") ## 两个字符串相加
@@ -12,6 +15,9 @@ print("abc" + "abc") ## 两个字符串相加
 5
 2
 6
+1.5
+1
+1
 9
 abcabcabc
 abcabc
--- 01-start/example/base_operation.py
+++ 01-start/example/base_operation.py
@@ -1,6 +1,10 @@
-print(3 + 2)
-print(3 - 1)
-print(3 * 2)
-print(3 ** 2)
-print("abc" * 3)
-print("abc" + "abc")
+# coding=utf-8
+print(3 + 2)  # 加法
+print(3 - 1)  # 减法
+print(3 * 2)  # 乘法
+print(10 / 3)  # 除法
+print(3 // 2)  # 整除
+print(3 % 2)  # 取余数
+print(3 ** 2)  ## 幂次计算
+print("abc" * 3)  ## abc打印三次
+print("abc" + "abc")  ## 两个字符串相加
